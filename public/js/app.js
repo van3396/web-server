48457e0ca9b66b9ff2bd864a987afad1a41c1550
Qui <quivan3396@yahoo.com>
2021-03-08 10:02:49 -0500

    feat: setup search form for location

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,12 @@
 console.log("Client side js is loaded!!!");
 
-fetch("http://localhost:3000/weather?address=boston").then((response) => {
+const weatherForm = document.querySelector("form");
+const search = document.querySelector('input')
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const location = search.value
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
   response.json().then((data) => {
     if (data.error) {
       console.log(data.error);
@@ -9,4 +15,5 @@ fetch("http://localhost:3000/weather?address=boston").then((response) => {
       console.log(data.forecast);
     }
   });
+});
 });
